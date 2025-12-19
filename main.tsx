645b0d8d0d5d587ad09227dd68eb93de0e86fdf9
escobarabogados1-prog <escobarabogados1@gmail.com>
2025-12-18 19:22:16 -0500

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -7,3 +7,13 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>
 );
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // <--- AGREGA ESTA LÍNEA AQUÍ
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
